482d798c00c48ad02eb7a8a1ac164dbb31ed29c6
Nick <nphuynh11@gmail.com>
2021-07-07 20:16:11 -0400

    views touchables images

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar, Image, TouchableNativeFeedback } from 'react-native';
 
 export default function App() {
+  const handlePress = () => console.log("Text pressed");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text onPress={handlePress}>Hello React Native</Text>
+      <TouchableNativeFeedback onPress={() => console.log("image touched")}>
+        <View style={{
+          width: 200,
+          height: 200,
+          backgroundColor: 'green',
+        }}></View>
+      </TouchableNativeFeedback>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    paddingTop: StatusBar.currentHeight || 0,
+    justifyContent: "center",
+    alignItems: "center"
   },
 });
