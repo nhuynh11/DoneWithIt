4e684a796d0a60e525fc20889511e09fe8b1a67f
Nick <nphuynh11@gmail.com>
2021-07-07 20:57:31 -0400

    android status bar padding

--- App.js
+++ App.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, StatusBar, Image, TouchableNativeFeedback } from 'react-native';
+import { Platform, StyleSheet, Text, View, SafeAreaView, StatusBar, Image, TouchableNativeFeedback, Button, Alert } from 'react-native';
 
 export default function App() {
   const handlePress = () => console.log("Text pressed");
   return (
     <SafeAreaView style={styles.container}>
-      <Text onPress={handlePress}>Hello React Native</Text>
-      <TouchableNativeFeedback onPress={() => console.log("image touched")}>
-        <View style={{
-          width: 200,
-          height: 200,
-          backgroundColor: 'green',
-        }}></View>
-      </TouchableNativeFeedback>
-      <StatusBar style="auto" />
+      <Button 
+      color="orange"
+      title="Click Me" 
+      onPress={() =>console.log("button pressed")} />
     </SafeAreaView>
   );
 }
@@ -22,8 +17,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-    paddingTop: StatusBar.currentHeight || 0,
-    justifyContent: "center",
-    alignItems: "center"
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
