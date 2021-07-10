80aeb7c7d8261347b2384013def10873dc3ed052
Nick <nphuynh11@gmail.com>
2021-07-10 15:39:56 -0400

    welcome and image screens

--- App.js
+++ App.js
@@ -1,24 +1,8 @@
-import React from 'react';
-import { Dimensions, Platform, StyleSheet, Text, View, SafeAreaView, StatusBar, Image, TouchableNativeFeedback, Button, Alert } from 'react-native';
-import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
+import React from "react";
+import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  const {landscape} = useDeviceOrientation();
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={{
-        backgroundColor: 'pink',
-        width: "100%",
-        height: landscape ? '100%' : '30%',
-      }}></View>
-    </SafeAreaView>
-  );
+  return <WelcomeScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
