246410fbc42414e4a500981e859f383a13b9c67c
Nick <nphuynh11@gmail.com>
2021-07-11 14:05:51 -0400

    welcom screen changes and button components

--- App.js
+++ App.js
@@ -2,7 +2,10 @@ import React from "react";
 import { View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import AppButton from "./app/components/AppButton";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    <WelcomeScreen />
+  );
 }
