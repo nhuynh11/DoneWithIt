84ad75919d0984580695853883c25c879bc8e625
Nick <nphuynh11@gmail.com>
2021-07-12 16:16:08 -0400

    account screen

--- App.js
+++ App.js
@@ -8,16 +8,11 @@ import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
+import AccountScreen from "./app/screens/AccountScreen";
 
 export default function App() {
   return (
-    <Screen>
-      <Icon
-        name="email"
-        size={50}
-        backgroundColor="red"
-        iconColor="white"
-      />
-    </Screen>
+    <AccountScreen />
   );
 }
--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -5,7 +5,14 @@ import AppText from "./AppText";
 import colors from "../config/colors";
 import Swipeable from "react-native-gesture-handler/Swipeable";
 
-function ListItem({ title, subTitle, image, onPress, renderRightActions }) {
+function ListItem({ 
+  title, 
+  subTitle, 
+  image, 
+  IconComponent, 
+  onPress, 
+  renderRightActions }) 
+{
   return (
     <Swipeable renderRightActions={renderRightActions}>
       <TouchableHighlight 
@@ -13,15 +20,15 @@ function ListItem({ title, subTitle, image, onPress, renderRightActions }) {
         underlayColor={colors.light}
       >
         <View style={styles.container}>
-        <Image style={styles.image} source={image} />
-        <View>
+        {IconComponent}
+        {image && <Image style={styles.image} source={image} />}
+        <View style={styles.detailsContainer}>
           <AppText style={styles.title}>{title}</AppText>
-          <AppText style={styles.subTitle}>{subTitle}</AppText>
+          {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
         </View>
       </View>
       </TouchableHighlight>
     </Swipeable>
-    
     
   );
 }
@@ -30,12 +37,16 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
     padding: 15,
+    backgroundColor: colors.white,
+  },
+  detailsContainer: {
+    marginLeft: 10,
+    justifyContent: 'center',
   },
   image: {
     width: 70,
     height: 70,
     borderRadius: 35,
-    marginRight: 10,
   },
   subTitle: {
     color: colors.medium,
