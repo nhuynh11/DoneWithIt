0a78a378a3212601e76263ade878849bd413f681
Nick <nphuynh11@gmail.com>
2021-07-13 16:33:38 -0400

    adding image picker to listing edit screen

--- App.js
+++ App.js
@@ -7,21 +7,10 @@ import { result } from "lodash";
 import { Button, Image } from "react-native";
 import ImageInput from "./app/components/ImageInput";
 import ImageInputList from "./app/components/ImageInputList";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 
 export default function App() {
 
-  const [imageUris, setImageUris] = useState([]);
-
-  const handleAdd = uri => {
-    setImageUris([...imageUris, uri]);
-  }
-
-  const handleRemove = uri => {
-    setImageUris(imageUris.filter(imageUri => imageUri !== uri));
-  }
-
-  return <Screen>
-    <ImageInputList imageUris={imageUris} onAddImage={handleAdd} onRemoveImage={handleRemove} />
-  </Screen>;
+  return <ListingEditScreen />
 }
