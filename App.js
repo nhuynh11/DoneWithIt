1009041352adab3339620cf4d5d699953dfe7af6
Nick <nphuynh11@gmail.com>
2021-07-13 15:11:16 -0400

    made image input component

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import * as Permissions from 'expo-permissions';
 import Screen from "./app/components/Screen";
 import { result } from "lodash";
 import { Button, Image } from "react-native";
+import ImageInput from "./app/components/ImageInput";
 
 
 export default function App() {
@@ -31,7 +32,6 @@ export default function App() {
   }
 
   return <Screen>
-    <Button title="Select Image" onPress={selectImage} />
-    <Image source={{ uri: imageUri}} style={{width: 200, height: 200 }} />
+    <ImageInput imageUri={imageUri} onChangeImage={(uri) => setImageUri(uri)} />
   </Screen>;
 }
