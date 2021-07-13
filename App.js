fdf18422a97563c332a569098e7600d253ca800d
Nick <nphuynh11@gmail.com>
2021-07-12 22:05:27 -0400

    login and listing edit screens and components

--- App.js
+++ App.js
@@ -13,22 +13,11 @@ import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
 import AppTextInput from "./app/components/AppTextInput";
 import AppPicker from "./app/components/AppPicker";
-
-const categories = [
-  { label: "Furniture", value: 1 },
-  { label: "Clothing", value: 2 },
-  { label: "Cameras", value: 3 },
-]
+import LoginScreen from "./app/screens/LoginScreen";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-
-  const [category, setCategory] = useState(categories[0]);
-
   return (
-    <Screen>
-      <AppPicker selectedItem={category} onSelectItem={item => setCategory(item)}
-        items={categories} icon="apps" placeholder="Category" />
-      <AppTextInput icon="email" placeholder="Email" />
-    </Screen>
+    <ListingEditScreen />
   );
 }
