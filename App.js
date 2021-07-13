9d43173cebf83be924960d3c5429c853ad737fb9
Nick <nphuynh11@gmail.com>
2021-07-13 16:09:39 -0400

    image input list component

--- App.js
+++ App.js
@@ -6,32 +6,22 @@ import Screen from "./app/components/Screen";
 import { result } from "lodash";
 import { Button, Image } from "react-native";
 import ImageInput from "./app/components/ImageInput";
+import ImageInputList from "./app/components/ImageInputList";
 
 
 export default function App() {
 
-  const [imageUri, setImageUri] = useState();
+  const [imageUris, setImageUris] = useState([]);
 
-  const requestPermission = async () => {
-    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
-    if (!granted) alert('you need to enable permission to access the library')
+  const handleAdd = uri => {
+    setImageUris([...imageUris, uri]);
   }
 
-  useEffect(() => {
-    requestPermission();
-  }, [])
-
-  const selectImage = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync();
-      if (!result.cancelled)
-        setImageUri(result.uri);
-    } catch (error) {
-      console.log('error reading an image', error)
-    } 
+  const handleRemove = uri => {
+    setImageUris(imageUris.filter(imageUri => imageUri !== uri));
   }
 
   return <Screen>
-    <ImageInput imageUri={imageUri} onChangeImage={(uri) => setImageUri(uri)} />
+    <ImageInputList imageUris={imageUris} onAddImage={handleAdd} onRemoveImage={handleRemove} />
   </Screen>;
 }
