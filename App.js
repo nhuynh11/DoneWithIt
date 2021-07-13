2ee9409c8e26d185c2a7824a33a2b72ebfacca94
Nick <nphuynh11@gmail.com>
2021-07-13 19:15:58 -0400

    practicing nesting stack and tab navigators

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Button, Text } from 'react-native';
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import Screen from "./app/components/Screen";
 
@@ -56,8 +57,20 @@ const Account = () => <Screen><Text>Acount</Text></Screen>
 
 const Tab = createBottomTabNavigator();
 const TabNavigator = () => (
-  <Tab.Navigator>
-    <Tab.Screen name = "Feed" component={Tweets} />
+  <Tab.Navigator
+    tabBarOptions={{
+      activeBackgroundColor: "tomato",
+      activeTintColor: "white",
+      inactiveBackgroundColor: "#eee",
+      inactiveTintColor: "black",
+    }}
+  >
+    <Tab.Screen 
+      name = "Feed" 
+      component={StackNavigator}
+      options={{
+        tabBarIcon: ({size, color}) => <MaterialCommunityIcons name="home" size={size} color={color} />
+      }} />
     <Tab.Screen name = "Account" component={Account} />
   </Tab.Navigator>
 )
@@ -66,7 +79,7 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <TabNavigator />
     </NavigationContainer>
   );
 }
