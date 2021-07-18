0ad689de69f10ea113e385372a6f2aea5ff2ddb9
Nick <nphuynh11@gmail.com>
2021-07-18 16:17:10 -0400

    implement user registration

--- app/hooks/useApi.js
+++ app/hooks/useApi.js
@@ -1,20 +1,20 @@
 import { useState } from "react";
 
 export default useApi = (apiFunc) => {
-    const [data, setData] = useState([]);
-    const [error, setError] = useState(false);
-    const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    const request = async (...args) => {
-        setLoading(true);
-        const response = await apiFunc(...args);
-        setLoading(false);
+  const request = async (...args) => {
+    setLoading(true);
+    const response = await apiFunc(...args);
+    setLoading(false);
 
-        if (!response.ok) return setError(true);
+    setError(!response.ok);
+    setData(response.data);
+    return response;
+  };
 
-        setError(false);
-        setData(response.data);
-    };
-
-    return { data, error, loading, request };
+  return { data, error, loading, request };
 };
+
--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import { StyleSheet } from "react-native";
 import * as Yup from "yup";
 
 import Screen from "../components/Screen";
+import usersApi from "../api/users";
+import authApi from "../api/auth";
+import useAuth from "../auth/useAuth";
 import {
   AppForm as Form,
   AppFormField as FormField,
   SubmitButton,
+  ErrorMessage
 } from "../components/forms";
+import useApi from "../hooks/useApi";
 
 const validationSchema = Yup.object().shape({
   name: Yup.string().required().label("Name"),
@@ -16,13 +21,38 @@ const validationSchema = Yup.object().shape({
 });
 
 function RegisterScreen() {
+  const registerApi = useApi(usersApi.register);
+  const loginApi = useApi(authApi.login);
+  const auth = useAuth();
+  const [error, setError] = useState();
+
+  const handleSubmit = async (userInfo) => {
+    const result = await registerApi.request(userInfo);
+
+    if (!result.ok) {
+      if (result.data) setError(result.data.error);
+      else {
+        setError("An unexpected error occurred.");
+        console.log(result);
+      }
+      return;
+    }
+
+    const { data: authToken } = await loginApi.request(
+      userInfo.email,
+      userInfo.password
+    );
+    auth.logIn(authToken);
+  };
+
   return (
     <Screen style={styles.container}>
       <Form
         initialValues={{ name: "", email: "", password: "" }}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={handleSubmit}
         validationSchema={validationSchema}
       >
+        <ErrorMessage error={error} visible={error} />
         <FormField
           autoCorrect={false}
           icon="account"
